255268ba2e6a08f0a5bf04d568a46f91f3152f85
Frédéric Thomachot <frederic.thomachot@campus-numerique-in-the-alps.com>
2018-07-23 11:51:25 +0200

    rattrapage vuejs : import des donnees

---
+++ src/components/Project.vue
@@ -0,0 +1,18 @@
+<template>  
+    <div  id="datasName">
+        <h1>{{title.name}}</h1>
+        <h2>Projet actif : {{title.isActive}}</h2>    
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Project',
+  props: ["title"],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
