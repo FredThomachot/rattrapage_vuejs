6bf0fcaeb101be56bda9afd312538ceef30ddaeb
Frédéric Thomachot <frederic.thomachot@campus-numerique-in-the-alps.com>
2018-07-27 11:39:39 +0200

    vuejs : full import api ok

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -2,8 +2,11 @@
 
  
       <div class="col-md-4">
-          <h2>Projet : {{projet.name}}</h2>
+          <img :src="projet.creator.picture">
+          <h3>{{projet.creator.name}}</h3>
+          <!-- <img src="https://s3.amazonaws.com/uifaces/faces/twitter/davidcazalis/128.jpg"> -->
           <h6>{{projet.description}}</h6>
+           <img :src="projet.collaborators.picture">
         
           <!-- <h3 v-if = "projet.isActive === true">actif</h3>  
           <h3 v-else class="nonActif">non actif</h3>
@@ -33,9 +36,9 @@ export default {
 
 .col-md-4 {
   background-color: aliceblue;
-  border-color: #42b983;
+  border-color:brown;
   border-style: solid;
-  border-width: 3px;
+  border-width: 1px;
   text-align:justify;
   margin: 5px;
 }
@@ -45,6 +48,10 @@ export default {
 }
 h6 {
   font-size: 14px;
+}
+img {
+  margin-top: 5px;
+  margin-bottom: 5px;
 }
 
 
